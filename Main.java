53eed05aef559bf5b23badf8b41f0442542a26c8
TinaD1906 <dotina87@gmail.com>
2024-10-04 10:17:30 -0700

    urmom

--- Main.java
+++ Main.java
@@ -6,8 +6,11 @@ public class Main
     }
     public static String lastFirstN(String s1, String s2, int n)
     {
-        String lengthS1 = s1.length();
-        String lengthS2 = s2.length();
-        String output = 
-    }
+        String first = s1.substring(0,n); //substring from 0 to n-1
+        int indexOfNthToLast = s2.length() - n;
+        String last = s2.substring(indexOfNthToLast); // starts at nth to last letter, and goes to the end 
+        System.out.println(first);
+        System.out.println(last);
+
+
 }
