f88b2c5ad32597b3344abfa65a4c41be261f049b
TinaD1906 <dotina87@gmail.com>
2024-10-07 10:08:42 -0700

    urmom

--- Main.java
+++ Main.java
@@ -2,15 +2,32 @@ public class Main
 {
     public static void main(String[]args)
     {
-
+        String word = lastFirstN("cream", "butter", 3);
+        System.out.println(word);
+        stringManip("Hello", "hello");
     }
+
     public static String lastFirstN(String s1, String s2, int n)
     {
-        String first = s1.substring(0,n); //substring from 0 to n-1
-        int indexOfNthToLast = s2.length() - n;
-        String last = s2.substring(indexOfNthToLast); // starts at nth to last letter, and goes to the end 
-        System.out.println(first);
-        System.out.println(last);
+  
+        int indexOfNthToLast = s1.length() - n;
+        String last = s1.substring(indexOfNthToLast); // starts at nth to last letter, and goes to the end 
+        String first = s2.substring(0,n); //substring from 0 to n-1
+        // System.out.println(first);
+        // System.out.println(last);
+        return last+first;
+    }
+
+    public static void stringManip(String s1, String s2)
+    {
+        System.out.println(s1.toUpperCase()); // makes entire word upper case 
+        String firstLetter = s2.substring(0,1); // isolate first letter in word
+        firstLetter = firstLetter.toUpperCase(); // make the first letter into upper case 
+
+        String lower = s2.toLowerCase(); //makes entire word lowercase 
+        String restOfs2 = s2.substring(1); // gets the rest of the word but the first letter 
+        System.out.println(firstLetter + restOfs2);
+    }
 
 
 }
